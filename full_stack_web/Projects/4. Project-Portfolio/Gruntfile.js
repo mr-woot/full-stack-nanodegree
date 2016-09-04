883bd72647961cf2bce336b24f8a854a11f12a63
mr-woot <tusharmudgal3@gmail.com>
2016-09-05 04:35:30 +0530

    project portfolio: image opti finished

--- full_stack_web/Projects/4. Project-Portfolio/Gruntfile.js
+++ full_stack_web/Projects/4. Project-Portfolio/Gruntfile.js
@@ -7,25 +7,39 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
-        concat: {
-            dist: {
-                src: config.jsSrcDir + '*.js',
-                dest: config.jsConcatDir + 'scripts.js',
-            }
+        responsive_images: {
+        	dev: {
+        		options: {
+        			engine: 'im',
+        			sizes: [{
+        				suffix: '_small',
+        				width: 320,
+        				quality: 40
+        			},
+        			{
+        				suffix: '_medium',
+        				width: 640,
+        				quality: 40
+        			},
+        			{
+        				suffix: '_large',
+        				width: 800,
+        				quality: 40
+        			},
+        			{
+        				suffix: '_x-large',
+        				width: 1600,
+        				quality: 40
+        			}]
+        		},
+        		files: [{
+        			expand: true,
+          			src: ['*.{gif,jpg,png,jpeg}'],
+        			cwd: 'img_src/',
+        			dest: 'img/'
+        		}]
+        	}
         },
-        jshint: {
-            options: {
-                "eqeqeq": true
-            },
-            all: [
-                'Gruntfile.js',
-                config.jsSrcDir + "*.js"
-            ]
-        }
     });
-
-    grunt.registerTask('default', [
-        'jshint',
-        'concat'
-    ]);
+    grunt.registerTask('default', ['responsive_images']);
 };
