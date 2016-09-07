61536229c4469098cd0f2b8deb5b5e8e1e210a1b
mr-woot <tusharmudgal3@gmail.com>
2016-09-07 17:12:47 +0530

    basic changed templating added

--- full_stack_web/2. Back-End/2. Templates/template/main.py
+++ full_stack_web/2. Back-End/2. Templates/template/main.py
@@ -1,28 +1,65 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import os
 import webapp2
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+form_html = """
+<form>
+<h2>Add Food</h2>
+<input type="text" name="food">
+%s
+<button>Add</button>
+</form>
+"""
+
+hidden_html = """
+<input type="hidden" name="food" value="%s">
+"""
+
+shopping_list_html = """
+<br>
+<br>
+<br>
+<h2>Shopping List</h2>
+<ul>
+%s
+</ul>
+"""
+
+items_html = """
+<li>%s</li>
+"""
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
 class MainPage(Handler):
     def get(self):
-        self.write("Hellow, Udacity!")
+        self.render("shopping_list.html", name=self.request.get("name"))
+        # output = form_html
+        # output_hidden = ""
+        # output_items = ""
+
+        # items = self.request.get_all("food")
+        # for item in items:
+        #     output_hidden += hidden_html % item
+        #     output_items += items_html % item
+
+        # output_shopping = shopping_list_html % output_items
+        # output += output_shopping
+        # output = output % output_hidden
+
+        # self.write(output)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage)],
