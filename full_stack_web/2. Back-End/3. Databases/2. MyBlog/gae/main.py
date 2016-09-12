2cdf94e13bacac0ae8a79cace9d466551a52a246
mr-woot <tusharmudgal3@gmail.com>
2016-09-13 03:09:28 +0530

    blog: project modified

--- full_stack_web/2. Back-End/3. Databases/2. MyBlog/gae/main.py
+++ full_stack_web/2. Back-End/3. Databases/2. MyBlog/gae/main.py
@@ -1,19 +1,23 @@
 import os
 import webapp2
 import jinja2
+from google.appengine.ext import db
 
 # Template dirs
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
+                               autoescape=True)
 
-# Regular expressions for authentification
-username_re = "^[a-zA-Z0-9_-]{3,20}$"
-password_re = "^.{3,20}$"
-email_re    = "^[\S]+@[\S]+.[\S]+$"
+# # Regular expressions for authentification
+# username_re = "^[a-zA-Z0-9_-]{3,20}$"
+# password_re = "^.{3,20}$"
+# email_re = "^[\S]+@[\S]+.[\S]+$"
 
 # Main handler that renders content
+
+
 class Handler(webapp2.RequestHandler):
+
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
@@ -24,14 +28,51 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class gae(Handler):
+
+class Post(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+
+class BlogPost(Handler):
+
+    def render_post(self, s_error="", c_error="", subject="", content=""):
+        self.render("add_post.html", s_error=s_error,
+                    c_error=c_error, subject=subject, content=content)
+
     def get(self):
-        self.render("index.html")
+        self.render_post()
+
     def post(self):
-    	text = self.request.get("text")
-        self.render("index.html", text=text)
+        error = False
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+        s_error = ""
+        c_error = ""
+        if not subject:
+            s_error = "Wanna go without subject, well not in my app"
+            error = True
+        if not content:
+            c_error = "It's not going without content, chica"
+            error = True
+        if error:
+            self.render_post(s_error, c_error, subject, content)
+        elif content and subject:
+            p = Post(subject=subject, content=content)
+            p.put()
+            self.redirect("/blog")
+
+
+class Blog(Handler):
+
+    def get(self):
+        posts = db.GqlQuery("select * from Post order by created desc")
+        self.render("index.html", posts=posts)
+
 
 app = webapp2.WSGIApplication([
-    ('/', gae)],
+    ('/', Blog),
+    ('/blog', Blog),
+    ('/post', BlogPost)],
     debug=True)
-
