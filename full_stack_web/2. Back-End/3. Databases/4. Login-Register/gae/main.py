97c5246f7c6e21ebd90d024e9346f7e8ca0d4ed6
mr-woot <tusharmudgal3@gmail.com>
2016-09-24 16:22:02 +0530

    Muli user blog started

--- full_stack_web/2. Back-End/3. Databases/4. Login-Register/gae/main.py
+++ full_stack_web/2. Back-End/3. Databases/4. Login-Register/gae/main.py
@@ -15,73 +15,6 @@ jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
 
-def blog_key(name='default'):
-    return db.Key.from_path('blogs', name)
-
-
-def render_post(response, post):
-    response.out.write('<b>' + post.subject + '</b><br>')
-    response.out.write(post.content)
-
-
-class Post(db.Model):
-    subject = db.StringProperty(required=True)
-    content = db.TextProperty(required=True)
-    created = db.DateTimeProperty(auto_now_add=True)
-    last_modified = db.DateTimeProperty(auto_now=True)
-
-    def render(self):
-        self._render_text = self.content.replace('\n', '<br>')
-        return render_str("post.html", p=self)
-
-
-class BlogFront(BlogHandler):
-
-    def get(self):
-        posts = greetings = Post.all().order('-created')
-        self.render('front.html', posts=posts)
-
-
-class PostPage(BlogHandler):
-
-    def get(self, post_id):
-        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
-        post = db.get(key)
-
-        if not post:
-            self.error(404)
-            return
-
-        self.render("permalink.html", post=post)
-
-
-class NewPost(BlogHandler):
-
-    def get(self):
-        if self.user:
-            self.render("newpost.html")
-        else:
-            self.redirect("/login")
-
-    def post(self):
-        if not self.user:
-            self.redirect('/blog')
-
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-
-        if subject and content:
-            p = Post(parent=blog_key(), subject=subject, content=content)
-            p.put()
-            self.redirect('/blog/%s' % str(p.key().id()))
-        else:
-            error = "subject and content, please!"
-            self.render("newpost.html", subject=subject,
-                        content=content, error=error)
-
-##########################################################################
-
-
 def make_salt(length=10):
     return ''.join(random.choice(letters) for x in xrange(length))
 
@@ -296,6 +229,77 @@ class Welcome(Handler):
             self.render('welcome.html', username=self.user.name)
         else:
             self.redirect('/signup')
+
+################################################################
+# blog's here
+
+
+def blog_key(name='default'):
+    return db.Key.from_path('blogs', name)
+
+
+def render_post(response, post):
+    response.out.write('<b>' + post.subject + '</b><br>')
+    response.out.write(post.content)
+
+
+class Post(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p=self)
+
+
+class BlogFront(Handler):
+
+    def get(self):
+        posts = greetings = Post.all().order('-created')
+        self.render('front.html', posts=posts)
+
+
+class PostPage(Handler):
+
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post=post)
+
+
+class NewPost(Handler):
+
+    def get(self):
+        if self.user:
+            self.render("newpost.html")
+        else:
+            self.redirect("/login")
+
+    def post(self):
+        if not self.user:
+            self.redirect('/blog')
+
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            p = Post(parent=blog_key(), subject=subject, content=content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            error = "subject and content, please!"
+            self.render("newpost.html", subject=subject,
+                        content=content, error=error)
+
+##########################################################################
+
 
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/signup', Register),
